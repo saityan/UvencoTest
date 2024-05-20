c5a46e704633a9ad0195c9333fe2042f3d3ca013
saityan <naminatosky@gmail.com>
2024-05-20 17:02:59 +0300

    fixed gradle issues

--- app/src/main/java/saityan/misc/uvencotest/model/CoffeeCup.kt
+++ app/src/main/java/saityan/misc/uvencotest/model/CoffeeCup.kt
@@ -7,7 +7,7 @@ import saityan.misc.uvencotest.util.Constants.COFFEE_CUPS_TABLE
 @Entity(tableName = COFFEE_CUPS_TABLE)
 data class CoffeeCup(
     @PrimaryKey(autoGenerate = true)
-    val id: String,
+    val id: Int,
 
     val name: String,
     val price: String,
